ad1a6c2d3ddb4e9c6622110b312db8dc55c76f3c
dbracero <davidbracero@gmail.com>
2022-04-16 15:41:29 +0200

    feat: added dictionary, colors, and some improvements

--- pywordle.py
+++ pywordle.py
@@ -1,14 +1,24 @@
+from operator import le
 import random
+import json
+from colorama import Fore, Back, Style
 
 LETTERS_COUNT: int = 5
 
 #TODO: Load words from file"
-words: list = ["angel", "pilas", "remar", "indio", "fugaz"]
-result: list = [None] * LETTERS_COUNT
+words: list = []
+miss: str = '[_]'
+result: list = [miss] * LETTERS_COUNT
 occurrences: dict = {}
+letter_counters: dict = {}
 guessed: bool = False
 
 MAX_ATTEMPTS: int = 6
+
+def load_dictionary() -> None:
+	with open('dict.json') as f:
+		global words
+		words = json.load(f)
 
 def load_word(word: str) -> None:
 	for letter in word:
@@ -18,35 +28,50 @@ def load_word(word: str) -> None:
 			occurrences[letter] += 1
 
 def check(attempt: str, word_to_guess: str) -> None:
-	partial_occurrences: dict = {}
+	letter_counters: dict = {}
+
+	for letter in attempt:
+		letter_counters[letter] = 0
+
 	for i in range(len(attempt)):
 		letter: str = attempt[i]
-		if letter not in partial_occurrences:
-			partial_occurrences[letter] = 1
+		if letter in word_to_guess:
+			letter_counters[letter] += 1
+			if letter_counters[letter] <= occurrences[letter]:
+				if letter == word_to_guess[i]:
+					#result[i] = '[' + letter + ']'
+					print(Fore.LIGHTGREEN_EX + '[' + letter + ']', end='')
+				else:
+					#result[i] = '(' + letter + ')'
+					print(Fore.LIGHTYELLOW_EX + '(' + letter + ')', end='')
+			else:
+				#result[i] = miss
+				print(Fore.LIGHTRED_EX + miss, end='')
 		else:
-			partial_occurrences[letter] += 1
-
-		if letter == word_to_guess[i]:
-			result[i] = "[" + letter + "]"
-		elif letter in word_to_guess:
-			if partial_occurrences[letter] <= occurrences[letter]:
-				result[i] = "(" + letter + ")"
-		else:
-			result[i] = "[_]"
-		
+			#result[i] = miss
+			print(Fore.LIGHTRED_EX + miss, end='')
+	print(Fore.RESET)
 
 if __name__ == '__main__':
+	load_dictionary()
 	word_to_guess: str = random.choice(words)
 	load_word(word_to_guess)
 
 	attempt_count: int = 0
 	while (attempt_count < MAX_ATTEMPTS and not guessed):
-		print("Write your guess...")
+		print('Write your guess...')
 		attempt: str = input()
+		while len(attempt) != 5:
+			print('Your guess must have 5 letters!')
+			attempt = input()
+		while attempt not in words:
+			print('Your word must be in the dictionary!')
+			attempt = input()
 		check(attempt, word_to_guess)
 		attempt_count += 1
 		if attempt == word_to_guess:
 			guessed = True
-			print("You guessed it!")
-		else:
-			print(result)
+			print('You guessed it!')
+		#else:
+			#print(result)
+	print('The word was: ' + Fore.GREEN + word_to_guess)
